f778bc37bc0f269cc5fa1e6c8b66d554715db7dc
Rashika <rashikajha19@gmail.com>
2020-04-28 22:50:02 +0530

    Using SwitchCase

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -6,35 +6,27 @@ public class Empywage {
 		double fullDay=8;
 		double halfDay=4;	
 		double empWage;
-		double monthDays=20;
-		
 		double empCheck=Math.floor(Math.random() * 10 ) %2;
-		
 		
 		switch ((int) empCheck){
 		case 1:
 			int IS_FULLTIME=1;
 			double fullTimeCheck=Math.floor(Math.random()*10)%2;
-			double totalMonthWage;
+
 			System.out.println("Employee is present");
 
 			switch ((int) fullTimeCheck){
 			case 1:
 				empWage=wagePerHour*fullDay;
-				totalMonthWage=empWage*monthDays;
 				System.out.println("Employee works fulltime and Employee wage is: "+ empWage);
-				System.out.println("Employee wage for a month is: "+ totalMonthWage);
 
 			break;
 
-			case 0:
+			default:
 				empWage=wagePerHour*halfDay;
-				totalMonthWage=empWage*monthDays;
                                 System.out.println("Employee works parttime and Employee wage is: "+ empWage);
-				System.out.println("Employee wage for a month is: "+ totalMonthWage);
 			break;
 			}
-		break;
 
 		case 0:
 			System.out.println("Employee is absent");
