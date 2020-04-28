80457ce85fd0a6a73f68dda463825b4d254a9219
Rashika <rashikajha19@gmail.com>
2020-04-28 22:32:35 +0530

    PartTime

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,22 +1,33 @@
 public class Empywage {
 	public static void main(String[] args){
 	
-		System.out.println("Welcome to the Employee wage computation");
 		int IS_PRESENT=1;
-		double wagePerHour=20;
-		double fullDay=8;	
-		double empWage;
-		double empCheck=Math.floor(Math.random() * 10 ) %2;
+				double wagePerHour=20;
+				double fullDay=8;
+				double halfDay=4;	
+				double empWage;
+				double empCheck=Math.floor(Math.random() * 10 ) %2;
+	
+
+				if (empCheck==IS_PRESENT){
+				int IS_FULLTIME=1;
+				double fullTimeCheck=Math.floor(Math.random()*10)%2;
+			
+				System.out.println("Employee is present");
+
+				if (fullTimeCheck==IS_FULLTIME){
+				empWage=wagePerHour*fullDay;
+				System.out.println("Employee works fulltime and Employee wage is: "+ empWage);
+				}
+				else {
+				empWage=wagePerHour*halfDay;
+                                System.out.println("Employee works parttime and Employee wage is: "+ empWage);
+				}
+			}
 		
 
-		if (empCheck==IS_PRESENT){
-			System.out.println("Employee is present");
-			empWage=wagePerHour*fullDay;
-			System.out.println("Employee wage is: "+ empWage);
-		}
 		else{
 			System.out.println("Employee is absent");
 		}
-		
 	}
 }
