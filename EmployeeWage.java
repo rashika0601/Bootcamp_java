35d7232a40ebf2002a601beae95d4056ca469a92
Rashika jha <61932570+rashika0601@users.noreply.github.com>
2020-04-29 21:20:32 +0530

    Add files via upload

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,32 +1,50 @@
-public class EmployeeWage {
+
+public class Empdailywage {
+			
 	public static void main(String[] args){
-	System.out.println("Welcome to the Employee wage computation");
+	
+		System.out.println("Welcome to the Employee wage computation");
 		int IS_PRESENT=1;
 		double wagePerHour=20;
 		double fullDay=8;
-		double halfDay=4;	
+		double halfDay=4;
 		double empWage;
+		double monthDays=20;
+		int days=1;
+
 		double empCheck=Math.floor(Math.random() * 10 ) %2;
 		
+
 		switch ((int) empCheck){
 		case 1:
 			int IS_FULLTIME=1;
 			double fullTimeCheck=Math.floor(Math.random()*10)%2;
-
+			double totalMonthWage;
 			System.out.println("Employee is present");
 
 			switch ((int) fullTimeCheck){
 			case 1:
 				empWage=wagePerHour*fullDay;
 				System.out.println("Employee works fulltime and Employee wage is: "+ empWage);
+				while (days<21){
+				totalMonthWage=empWage*days;
+				System.out.println("Employee wage for "+days+" day is: "+ totalMonthWage);
+				days++;
+				}
 
 			break;
 
-			default:
+			case 0:
 				empWage=wagePerHour*halfDay;
-                                System.out.println("Employee works parttime and Employee wage is: "+ empWage);
+				System.out.println("Employee works parttime and Employee wage is: "+ empWage);
+				while (days<21){
+				totalMonthWage=empWage*days;
+				System.out.println("Employee wage for "+days+" day is: "+ totalMonthWage);
+				days++;
+				}
 			break;
 			}
+		break;
 
 		case 0:
 			System.out.println("Employee is absent");
