1d1512d4a9362671f58d41c6b702065c84ec3b37
Rashika jha <61932570+rashika0601@users.noreply.github.com>
2020-04-30 12:36:59 +0530

    Employee_wage_function

--- DAY22/EmployeeWage.java
+++ DAY22/EmployeeWage.java
@@ -1,39 +1,35 @@
 
-public class Empdailywage {
-			
-	public static void main(String[] args){
+class EmployeWage{
+    static final int IS_PART_TIME=1; 
+	static final int IS_FULL_TIME=2; 
+	static final int MAX_HRS_IN_MONTH=10; 
+	static final int EMP_RATE_PER_HR=20; 
+	static final int NUM_WORKING_DAYS=20;
 	
-		System.out.println("Welcome to the Employee wage computation Program");
-	   
-	
-	    int IS_PART_TIME=1;
-		int IS_FULL_TIME=2;
-		int MAX_HRS_IN_MONTH=100; 
-		int EMP_RATE_PER_HR=20; 
-		int NUM_WORKING_DAYS=20;
-		int totalEmpHr=0; 
-		int totalWorkingDays=0;
-
-		while(totalEmpHr <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS)
-		{
-			totalWorkingDays++; 
-			int attendance=(int)(Math.random()*3); 
-			int empHrs=0;
-			switch(attendance) 
-			{ case 1:
-				empHrs=8; 
-				break; 
-			case 2:
-				empHrs=4;
-				break;
-			default:
-				empHrs=0;
-				break; 
-			} 
-			totalEmpHr=(totalEmpHr+empHrs); 
-		} 
-		int totalSalary=(totalEmpHr*EMP_RATE_PER_HR);
-		System.out.println("Salary Of Employee= "+totalSalary );
-
+	public static int caclEmpWageForCompany(String company,int empRate,int numOfDays,int maxHrs) {
+		int empHrs=0, totalEmpHrs=0,totalWorkingDays=0;
+		while(totalEmpHrs <= maxHrs && totalWorkingDays < numOfDays) {
+			 totalWorkingDays++; 
+			 int attendance=(int)(Math.random()*3); 
+			 switch(attendance) 
+			 { case IS_FULL_TIME:empHrs=8;
+			 break; 
+			 case IS_PART_TIME:empHrs=4;
+			 break;
+			 default:empHrs=0; 
+			 break; 
+			 } 
+			 totalEmpHrs=(totalEmpHrs+empHrs); 
+			 System.out.println("Days= "+ totalWorkingDays+ " Emp Hr: "+empHrs);
+		}
+			 int totalEmpWage=(totalEmpHrs*empRate);
+			  System.out.println("Total Emp Wage for Company "+company+" is ="+totalEmpWage );
+			 
+		return totalEmpWage;
+		
+	}
+public static void main(String[] args) 
+    {
+	   int totalWage=caclEmpWageForCompany("dmart",20,2,10);    
      }
 }
