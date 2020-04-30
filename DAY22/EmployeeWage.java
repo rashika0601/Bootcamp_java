5266cd7706ba85bf21840ff186b3136ca9c41b25
Rashika jha <61932570+rashika0601@users.noreply.github.com>
2020-04-30 12:19:15 +0530

    PartTime Wage

--- DAY22/EmployeeWage.java
+++ DAY22/EmployeeWage.java
@@ -4,17 +4,30 @@ public class Empdailywage {
 	public static void main(String[] args){
 	
 		System.out.println("Welcome to the Employee wage computation Program");
-	int attendance=(int)(Math.random()*2); 
-		int salary; 
-                int empRatePerHr=20; 
-	        int empHrs=8; 
-		if (attendance == 1) 
-                  salary=empHrs*empRatePerHr ; 
-	        else
-		  salary=0;
-                    
-                System.out.println("Daily Wage of an Employee= " +salary);    
+	    int isPartTime=1; 
+		int isFullTime=2; 
+		int empRatePerHr=20; 
+		int empHrs=0;
+		int attendance=(int)(Math.random()*3); 
+		int salary=0;
+
+		switch(attendance)
+		{ 
+		case 1:
+			empHrs=8; 
+		  break;
+		case 2: 
+			empHrs=4; 
+			break;
+		default: 
+			empHrs=0; 
+			break; 
+		}
+
+		salary=empHrs*empRatePerHr ;
+
+		System.out.println("Salary Of Employee= "+salary );
+
      }
 }
-
 		
