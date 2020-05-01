54d392d41e8d4efe76f3bec8b3ba1d6e24293ad2
Rashika jha <61932570+rashika0601@users.noreply.github.com>
2020-05-01 15:12:45 +0530

    Monthly wage with added Comment

--- DAY22/EmployeeWage.java
+++ DAY22/EmployeeWage.java
@@ -1,18 +1,18 @@
 
-public class Empdailywage {
-			
+public class Empywage {
 	public static void main(String[] args){
-	
-		System.out.println("Welcome to the Employee wage computation Program");
-	   
-		int isPartTime=1; 
+	System.out.println("Welcome to the Employee wage computation");
+	// Declare all data
+	    int isPartTime=1; 
 		int isFullTime=2; 
 		int empRatePerHr=20;
 		int totalSalary=0;
 		int empHrs=0;
 		int numWorkingDays=20; 
+		// checking the condition of working day of an employee
 		for(int day=1; day<=numWorkingDays;day++) 
 		{
+			// check attandance
 			int attendance=(int)(Math.random()*3); 
 			int salary=0;
 			switch(attendance) 
@@ -27,8 +27,10 @@ public class Empdailywage {
 				empHrs=0;
 				break;
 			}
-
+			//calculating salary of an employee for this month
 			salary=empHrs*empRatePerHr ; totalSalary=(totalSalary+salary ); }
 		System.out.println("Salary Of Employee= "+totalSalary );
      }
 }
+
+		
