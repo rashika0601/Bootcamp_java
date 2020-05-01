ee1777bc1d8e1c6e00959a4711da34bcf9d5ae3a
Rashika jha <61932570+rashika0601@users.noreply.github.com>
2020-05-01 15:21:41 +0530

    dailywage

--- DAY22/EmployeeWage.java
+++ DAY22/EmployeeWage.java
@@ -1,20 +1,19 @@
-
-public class Empdailywage {
-			
-	public static void main(String[] args){
-	
-		System.out.println("Welcome to the Employee wage computation Program");
+public class EmployeWage{
+public static void main(String[] args) 
+    {
+     System.out.println("Welcome to the Employee wage computation");    	
+     // decleartion
 	int attendance=(int)(Math.random()*2); 
-		int salary; 
-                int empRatePerHr=20; 
-	        int empHrs=8; 
+	int salary; 
+    int empRatePerHr=20; 
+	int empHrs=8; 
+	// Checking attandance
 		if (attendance == 1) 
-                  salary=empHrs*empRatePerHr ; 
+			// Calculating salary according to attandance and working hrs and rate per hrs
+            salary=empHrs*empRatePerHr ; 
 	        else
 		  salary=0;
                     
                 System.out.println("Daily Wage of an Employee= " +salary);    
      }
 }
-
-		
