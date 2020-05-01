9bd2d4fad2cdb099f034964953ca732f5e59b334
Rashika jha <61932570+rashika0601@users.noreply.github.com>
2020-05-01 15:20:11 +0530

    Till working Day and Hours

--- DAY22/EmployeeWage.java
+++ DAY22/EmployeeWage.java
@@ -1,22 +1,21 @@
-
-public class Empdailywage {
-			
-	public static void main(String[] args){
-	
-		System.out.println("Welcome to the Employee wage computation Program");
-	   
-	
-	    int IS_PART_TIME=1;
-		int IS_FULL_TIME=2;
-		int MAX_HRS_IN_MONTH=100; 
-		int EMP_RATE_PER_HR=20; 
-		int NUM_WORKING_DAYS=20;
-		int totalEmpHr=0; 
-		int totalWorkingDays=0;
+public class EmployeWage{
+public static void main(String[] args) 
+    {
+    	System.out.println("Welcome to the Employee Wage Computation program");
+    	//declartion
+	     private static final int IS_PART_TIME=1;
+		 private static final int IS_FULL_TIME=2;
+		 private static final int MAX_HRS_IN_MONTH=100; 
+		 private static final int EMP_RATE_PER_HR=20; 
+		 private static final int NUM_WORKING_DAYS=20;
+		 private static final int totalEmpHr=0; 
+		 private static final int totalWorkingDays=0;
+		//check the condition if the employee are working maxm_hrs & no.of working day
 
 		while(totalEmpHr <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS)
 		{
 			totalWorkingDays++; 
+			//checking attandance
 			int attendance=(int)(Math.random()*3); 
 			int empHrs=0;
 			switch(attendance) 
@@ -32,6 +31,7 @@ public class Empdailywage {
 			} 
 			totalEmpHr=(totalEmpHr+empHrs); 
 		} 
+		//calculating salary of an employee
 		int totalSalary=(totalEmpHr*EMP_RATE_PER_HR);
 		System.out.println("Salary Of Employee= "+totalSalary );
 
